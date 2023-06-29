f74eddbd48e854807a7559785e34473c5d94712b
Edwin Castellanos <gnuedwin@gmail.com>
2023-06-29 11:41:57 -0400

    feat Crear Proyecto

---
+++ CentroEstetica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CentroEstetica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
