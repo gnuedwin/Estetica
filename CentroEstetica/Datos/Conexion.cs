1e9647590edbeb6dbc2bbe5e0a727f5f583c47e3
Edwin Castellanos <gnuedwin@gmail.com>
2023-06-29 12:11:41 -0400

    feat(conexion.cs)

---
+++ CentroEstetica/Datos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+namespace CentroEstetica.Datos
+{
+    public class Conexion
+    {  
+        private string cadenaSQl = string.Empty;
+        public Conexion()
+        {
+            var builder = new ConfigurationBinder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsetting.json").Build();
+        }
+
+    }
+}
