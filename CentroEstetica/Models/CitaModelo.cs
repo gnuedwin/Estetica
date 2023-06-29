9d08aa6771929c58efa361cf305009c34e9475f2
Edwin Castellanos <gnuedwin@gmail.com>
2023-06-29 11:52:19 -0400

    feat(Modelo)

---
+++ CentroEstetica/Models/CitaModelo.cs
@@ -0,0 +1,11 @@
+﻿namespace CentroEstetica.Models
+{
+    public class CitaModelo
+    {
+        public int IdCliente { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public string? Tratamiento { get; set; } 
+
+    }
+}
---
+++ CentroEstetica/Models/ClienteModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CentroEstetica.Models
+{
+    public class ClienteModel
+    { 
+        public int IdCliente { get; set; }
+        public string? Nombre { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; } 
+
+
+    }
+}
